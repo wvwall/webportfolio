3ff0dfbb6c685e7d08b7217043faa72e99509fa0
wvwall <waltervelardo@gmail.com>
2021-06-03 15:30:02 +0200

    Project and contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 $(".hamburger_menu").click(function () {
   $(".sidebar").toggleClass("open")
 });
+
+$("a").click(function () {
+  $(".sidebar").removeClass("open")
+});
+
+ $('a').click(function() {
+  $('.active').removeClass('active');
+  $(this).addClass('active');
+});
+ 
