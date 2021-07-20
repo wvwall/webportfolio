f73f541553ffbd6d748b49cd60c24bf5f9b4de13
Walter Velardo <waltervelardo@MacBook-Pro.station>
2021-07-20 13:24:16 +0200

    active_scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,33 @@
-$(".hamburger_menu").click(function () {
-  $(".sidebar").toggleClass("open")
-});
 
-$("a").click(function () {
-  $(".sidebar").removeClass("open")
-});
+$(document).ready(function() {
 
- $('a').click(function() {
-  $('.active').removeClass('active');
-  $(this).addClass('active');
-});
+    $(document).on("scroll", onScroll);
+
+    function onScroll(event){
+        var scrollPos = $(document).scrollTop();
+        $('a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    }
+    $(".hamburger_menu").click(function () {
+        $(".sidebar").toggleClass("open")
+    });
+
+    $("a").click(function () {
+        $(".sidebar").removeClass("open")
+    });
+
+    $('a').click(function () {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+})
  
